3b8dddb13ebb74a33f229154b2a95126a8465e2e
Mathis Brongniart <spidarstudiogames@gmail.com>
2025-04-04 01:26:35 +0200

    Ajout fonctionstoggleAnswer et changeUpdateDate

--- scripts/interaction.js
+++ scripts/interaction.js
@@ -10,3 +10,20 @@ function toggleAnswer(element) {
         arrow.style.transform = 'rotate(90deg)';
     }
 }
+
+function changeUpdateDate(element) {
+    // Sélectionnez l'élément avec la classe 'maj-auto'
+    const dateElement = element.querySelector('.maj-auto');
+    if (dateElement) {
+        // Mettre à jour le contenu avec la date fixe
+        dateElement.innerHTML = '<b>04/04/2025 - 01h27</b>';
+    }
+}
+
+window.onload = function() {
+    // Appeler la fonction pour l'élément avec l'ID 'class'
+    const element = document.getElementById('class');
+    if (element) {
+        changeUpdateDate(element);
+    }
+};
